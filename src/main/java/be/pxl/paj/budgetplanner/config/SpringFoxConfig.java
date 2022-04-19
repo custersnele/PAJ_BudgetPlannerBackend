424036b965a18e4bfb2637f3028d6720598b1e8e
Nele Custers <custersnele@gmail.com>
2022-04-19 18:53:56 +0200

    update versions

--- src/main/java/be/pxl/paj/budgetplanner/config/SpringFoxConfig.java
+++
@@ -1,9 +0,0 @@
-package be.pxl.paj.budgetplanner.config;
-
-import org.springframework.context.annotation.Configuration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-@Configuration
-@EnableSwagger2
-public class SpringFoxConfig {                                    
-}
